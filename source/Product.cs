e53483447d45223b58e8624fc97b15eb650d01f7
Sergey-Sergeev <112146784+Sergey-Sergeev@users.noreply.github.com>
2025-10-02 23:35:22 +1000

    Reapply "add new"
    This reverts commit 76f40c0f90f6b1fa025f33a09dbe3d27572fd029.

---
+++ source/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinancialManager.source
+{
+    public class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }   
+        public int Count { get; private set; }
+        public ProductCategory Category { get; set; }
+
+
+        public Product(string Name, double Price, int Count, ProductCategory Category)
+        {
+            this.Name = Name;
+            this.Price = Price;
+            this.Count = Count;
+            this.Category = Category;
+        }
+
+    }
+}
